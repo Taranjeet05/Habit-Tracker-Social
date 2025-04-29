f57cf3d295948076b8505af68641a27b8de89c61
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-29 18:05:35 +0200

    Add User model with schema definition for user data

---
+++ BACKEND/src/models/User.ts
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    emailNotification: {
+      type: Boolean,
+      default: true,
+    },
+    theme: {
+      type: String,
+      enum: ["light", "dark", "system"],
+      default: "system",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
