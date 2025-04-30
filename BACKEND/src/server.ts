657fd434f7678ce40c08c34b6c26186ec11440d8
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-30 16:29:14 +0200

    Refactor server setup to streamline route imports and middleware configuration

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -9,6 +9,12 @@ import { connectDB } from "./config/mongoose-connection.js";
 connectDB();
 import debug from "debug";
 const log = debug("app:server");
+import index from "./routes/index.js";
+import usersRouter from "./routes/usersRouter.js";
+import habitsRouter from "./routes/habitsRouter.js";
+import habitLogsRouter from "./routes/habitLogsRouter.js";
+import friendRequestsRouter from "./routes/friendRequestsRouter.js";
+import notificationsRouter from "./routes/notificationsRouter.js";
 
 //  __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -25,9 +31,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Basic route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Habit Tracker Backend is running! 🚀");
-});
+
+app.use("/", index);
+app.use("/api/users", usersRouter);
+app.use("/api/habits", habitsRouter);
+app.use("/api/habit-logs", habitLogsRouter);
+app.use("/api/friends", friendRequestsRouter);
+app.use("/api/notifications", notificationsRouter);
 
 // Server setup
 const PORT = process.env.PORT || 3000;
