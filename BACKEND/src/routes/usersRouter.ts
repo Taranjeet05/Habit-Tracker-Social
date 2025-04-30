e2cb34583c00f9e319d607295928cb4ee5f2362b
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-04-30 20:45:00 +0200

    Add user registration route to usersRouter

--- BACKEND/src/routes/usersRouter.ts
+++ BACKEND/src/routes/usersRouter.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
+import { registerUser } from "../controllers/usersController.js";
 
+// Test route
 router.get("/", (req: Request, res: Response) => {
   res.send("Hello form the Users Router");
 });
+
+// User registration route
+router.post("/register", registerUser);
 
 export default router;
