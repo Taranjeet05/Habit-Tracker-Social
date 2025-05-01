3a7d070d08384d4f299d9bf59616996bffd2c7dd
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-05-01 21:56:12 +0200

    Refactor comments in usersRouter to clarify endpoint descriptions

--- BACKEND/src/routes/usersRouter.ts
+++ BACKEND/src/routes/usersRouter.ts
@@ -2,17 +2,13 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 import { loginUser, registerUser } from "../controllers/usersController.js";
 
-// Endpoint -> "/api/users"
-
 // Test route
 router.get("/", (req: Request, res: Response) => {
   res.send("Hello from the Users Router");
 });
 
-// User registration route
+// Endpoint -> "/api/users"
 router.post("/register", registerUser);
-
-// User login route
 router.post("/login", loginUser);
 
 export default router;
