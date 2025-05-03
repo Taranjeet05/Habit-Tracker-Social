3d5adfb1718c7c926701634e8fa63863c74587a3
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-05-03 18:40:12 +0200

    refactor: reorganize habitsRouter to improve code structure and clarity

--- BACKEND/src/routes/habitsRouter.ts
+++ BACKEND/src/routes/habitsRouter.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from "express";
+import { createHabit } from "../controllers/habitsController.js";
 const router = express.Router();
 
+// Test route
 router.get("/", (req: Request, res: Response) => {
   res.send("Hello from the habitsRouter");
 });
+
+// Endpoint -> "/api/habits"
+router.post("/", createHabit);
 
 export default router;
