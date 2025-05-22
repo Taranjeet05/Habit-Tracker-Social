c24f5b9e677271cf1999be9529ebcbfb90cebd22
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-05-22 19:57:17 +0200

    fix: remove unused Request and Response imports from habitLogsRouter

--- BACKEND/src/routes/habitLogsRouter.ts
+++ BACKEND/src/routes/habitLogsRouter.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { createHabitLog } from "../controllers/habitLogsController.js";
 const router = express.Router();
 
@@ -11,4 +11,3 @@ router.delete("/:id", deleteHabitLog);
 
 */
 export default router;
-
