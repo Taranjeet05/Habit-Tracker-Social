069cd5aa297c58e1e6a722ef7cbe16308e188958
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2025-06-14 20:02:17 +0200

    feat: add calculateStreakForHabit endpoint with user authentication and error handling

--- BACKEND/src/controllers/habitLogsController.ts
+++ BACKEND/src/controllers/habitLogsController.ts
@@ -549,3 +549,25 @@ export const deleteHabitLogById = async (
     });
   }
 };
+
+// calculateStreakForHabit	Calculate current streak like Snapchat (🔥)
+
+export const calculateStreakForHabit = async (req: Request, res: Response) => {
+  try {
+    // check if the user exsist or not 
+    const userId = req.user?._id || "6813a52286c4475597e179c6";
+    if(!userId) {
+      res.status(401).json({message : 'You Need to Login First'});
+      return;
+    }
+    //
+
+  } catch (error: unknown) {
+    const errorMessage = error instanceof Error ? error.message : undefined;
+    log(`Error while Calculate current streak`);
+    res.status(500).json({
+      message: 'System Error during Calculate current streak',
+      error: process.env.NODE_ENV === 'develoment' ? errorMessage : undefined
+    })
+  }
+}
