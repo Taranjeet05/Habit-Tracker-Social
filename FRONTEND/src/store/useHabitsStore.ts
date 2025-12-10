42302041413537cbe94b62677d237367dcbbec39
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-12-10 06:27:30 +0100

    Changed the useHabitsStore according to the new logic written for fetching the habits from backend

--- FRONTEND/src/store/useHabitsStore.ts
+++ FRONTEND/src/store/useHabitsStore.ts
@@ -1,61 +1,24 @@
 import { create } from "zustand";
-import { Habit } from "../types/Habit";
+import { Habit } from "../api/habitsApi";
 
-interface HabitsStoreState {
-  habits: Habit[]; // Cache UI list
-  selectedHabitId: string | null; // For viewing and editing 1 habit
-  filter: "all" | "active" | "archived"; // For filtering habits on UI
-  isCreateModalOpen: boolean;
-  isEditModalOpen: boolean;
+interface HabitsState {
+  todayHabits: Habit[];
+  dashboardHabits: Habit[];
+  isTodayLoaded: boolean;
+  isDashboardLoaded: boolean;
 
-  // --- Actions ---
-  setHabits: (habits: Habit[]) => void;
-  addHabit: (habit: Habit) => void;
-  updateHabit: (habitId: string, updatedData: Partial<Habit>) => void;
-  deleteHabit: (habitId: string) => void;
-  selectHabit: (habitId: string | null) => void;
-  setFilter: (filter: "all" | "active" | "archived") => void;
-  toggleCreateModal: (open?: boolean) => void;
-  toggleEditModal: (open?: boolean) => void;
+  setTodayHabits: (habits: Habit[]) => void;
+  setDashboardHabits: (habits: Habit[]) => void;
 }
 
-export const useHabitStore = create<HabitsStoreState>((set) => ({
-  habits: [],
-  selectedHabitId: null,
-  filter: "all",
-  isCreateModalOpen: false,
-  isEditModalOpen: false,
+export const useHabitsStore = create<HabitsState>((set) => ({
+  todayHabits: [],
+  dashboardHabits: [],
+  isTodayLoaded: false,
+  isDashboardLoaded: false,
 
-  setHabits: (habits) => set({ habits }),
-
-  addHabit: (habit) =>
-    set((state) => ({
-      habits: [...state.habits, habit],
-    })),
-
-  updateHabit: (habitId, updatedData) =>
-    set((state) => ({
-      habits: state.habits.map((habit) =>
-        habit._id === habitId ? { ...habit, ...updatedData } : habit
-      ),
-    })),
-
-  deleteHabit: (habitId) =>
-    set((state) => ({
-      habits: state.habits.filter((habit) => habit._id !== habitId),
-    })),
-
-  selectHabit: (habitId) => set({ selectedHabitId: habitId }),
-
-  setFilter: (filter) => set({ filter }),
-
-  toggleCreateModal: (open) =>
-    set((state) => ({
-      isCreateModalOpen: open ?? !state.isCreateModalOpen,
-    })),
-
-  toggleEditModal: (open) =>
-    set((state) => ({
-      isEditModalOpen: open ?? !state.isEditModalOpen,
-    })),
+  setTodayHabits: (habits) =>
+    set(() => ({ todayHabits: habits, isTodayLoaded: true })),
+  setDashboardHabits: (habits) =>
+    set(() => ({ dashboardHabits: habits, isDashboardLoaded: true })),
 }));
