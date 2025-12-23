3b686efc8947ba40d8720d8af41387ea1d7634c5
taranjeet <taranjeetsinghtanu2@gmail.com>
2025-12-23 18:14:09 +0100

    Added constant for the HabitForm component

---
+++ FRONTEND/src/components/Habits/Habit-Form/Constants.ts
@@ -0,0 +1,71 @@
+
+export const colorOptions = [
+  {
+    name: "Green",
+    value: "green",
+    bg: "bg-green-500",
+    ring: "ring-green-500",
+    border: "border-green-500",
+  },
+  {
+    name: "Blue",
+    value: "blue",
+    bg: "bg-blue-500",
+    ring: "ring-blue-500",
+    border: "border-blue-500",
+  },
+  {
+    name: "Purple",
+    value: "purple",
+    bg: "bg-purple-500",
+    ring: "ring-purple-500",
+    border: "border-purple-500",
+  },
+  {
+    name: "Teal",
+    value: "teal",
+    bg: "bg-teal-500",
+    ring: "ring-teal-500",
+    border: "border-teal-500",
+  },
+  {
+    name: "Orange",
+    value: "orange",
+    bg: "bg-orange-500",
+    ring: "ring-orange-500",
+    border: "border-orange-500",
+  },
+  {
+    name: "Red",
+    value: "red",
+    bg: "bg-red-500",
+    ring: "ring-red-500",
+    border: "border-red-500",
+  },
+  {
+    name: "Yellow",
+    value: "yellow",
+    bg: "bg-yellow-500",
+    ring: "ring-yellow-500",
+    border: "border-yellow-500",
+  },
+];
+
+export const frequencyOptions = [
+  { name: "Daily", value: "daily" },
+  { name: "Weekly", value: "weekly" },
+  { name: "Monthly", value: "monthly" },
+  { name: "Custom", value: "custom" },
+];
+
+export const daysOfWeek = [
+  { name: "Sun", value: 0 },
+  { name: "Mon", value: 1 },
+  { name: "Tue", value: 2 },
+  { name: "wed", value: 3 },
+  { name: "Thu", value: 4 },
+  { name: "Fri", value: 5 },
+  { name: "Sat", value: 6 },
+];
+
+export const MAX_REMINDERS_PER_DAY = 10;
